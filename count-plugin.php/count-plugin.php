06f6a01bc0b00df8a86396fc911572fcbd68137d
Eliza <itzjusteliza@gmail.com>
2020-11-19 15:31:27 +0100

    Removing unnecessary code and comments, adding descriptive comments

--- count-plugin.php/count-plugin.php
+++ count-plugin.php/count-plugin.php
@@ -10,22 +10,9 @@ Description: A Plugin made for a school project that takes a string as a paramet
  * Denna funktion tar en sträng som parameter och returnerar true om strängen är precis sju tecken lång.
 */
 
-// defined('ABSPATH') or die ("Nope.");
-
-// function is_seven_letters($str){
-//     if (strlen($str) === 7) {
-//         Echo "The string contains 7 letters!";
-//     } else {
-//         echo "The string contains more o.";
-//     }
-// }
-
-// is_seven_letters('okrtysd');
-
-
 defined('ABSPATH') or die ("Nope.");
 
-
+// countPlugin is a class that checks a string for its length and returns another string based on if the length is seven characters or not.
 class countPlugin {
 
         function activate() {
@@ -37,13 +24,13 @@ class countPlugin {
         function uninstall() {
         
     }
-        function checks_string_letters($str){
+        function checks_string_characters($str){
             if (strlen($str) === 7) {
-                echo "The string contains 7 characters!";
+                echo "<h1> The string contains 7 characters! </h1>";
             } else {
-                echo "The string contains more or less characters than 7.";
+                echo "<h1> The string contains more or less characters than 7. </h1>";
             }
-}
+    }
 }
 
 //Checks for the class 'countPlugin', then creates a new plugin instance and doesn't display content on Dashboard.
@@ -60,6 +47,6 @@ register_deactivation_hook( __FILE__, array($newCountPlugin, 'deactivate'));
 
 }
     if(!is_admin()) { // !is_admin makes content not display on Dashboard by returning false while on Dashboard
-            $newCountPlugin->checks_string_letters('1234567'); // Enqueues styles and scripts (style.css and script.js)
+            $newCountPlugin->checks_string_characters('1234567'); // Gives parameter $str a value which is checked in checks_string_characters.
     }
 ;
