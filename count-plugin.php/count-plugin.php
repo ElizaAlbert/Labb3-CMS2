63112af699941dc801b03d42c101a97fb59cfc96
Eliza <itzjusteliza@gmail.com>
2020-11-19 15:26:38 +0100

    Convertin Count plugin to OOP, adding activation and deactivation hooks.

--- count-plugin.php/count-plugin.php
+++ count-plugin.php/count-plugin.php
@@ -10,14 +10,56 @@ Description: A Plugin made for a school project that takes a string as a paramet
  * Denna funktion tar en sträng som parameter och returnerar true om strängen är precis sju tecken lång.
 */
 
+// defined('ABSPATH') or die ("Nope.");
+
+// function is_seven_letters($str){
+//     if (strlen($str) === 7) {
+//         Echo "The string contains 7 letters!";
+//     } else {
+//         echo "The string contains more o.";
+//     }
+// }
+
+// is_seven_letters('okrtysd');
+
+
 defined('ABSPATH') or die ("Nope.");
 
-function is_seven_letters($str){
-    if (strlen($str) === 7) {
-        Echo "The string contains 7 letters!";
-    } else {
-        echo "The string contains more or less letters than 7.";
+
+class countPlugin {
+
+        function activate() {
+            echo "The plugin was activated.";
     }
+        function deactivate() {
+            echo "The plugin was deactivated.";
+    }
+        function uninstall() {
+        
+    }
+        function checks_string_letters($str){
+            if (strlen($str) === 7) {
+                echo "The string contains 7 characters!";
+            } else {
+                echo "The string contains more or less characters than 7.";
+            }
+}
 }
 
-is_seven_letters('Not seven letters.');
+//Checks for the class 'countPlugin', then creates a new plugin instance and doesn't display content on Dashboard.
+if (class_exists('countPlugin')) { 
+        $newCountPlugin = new countPlugin();
+        
+//activation
+register_activation_hook( __FILE__, array($newCountPlugin, 'activate'));
+
+//deactivation
+register_deactivation_hook( __FILE__, array($newCountPlugin, 'deactivate'));
+
+//uninstall
+
+}
+    if(!is_admin()) { // !is_admin makes content not display on Dashboard by returning false while on Dashboard
+            $newCountPlugin->checks_string_letters('1234567'); // Enqueues styles and scripts (style.css and script.js)
+    }
+;
